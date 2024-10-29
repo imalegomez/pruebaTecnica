f569531d6789ec650dcf5e7e96a0f0218f67c637
imalegomez <imalegomez2004@gmail.com>
2024-10-29 00:34:57 -0300

    funciones de validaciones para sudoku agregadas en SudokuController

--- api/app/Http/Controllers/SudokuController.php
+++ api/app/Http/Controllers/SudokuController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SudokuGame;
-use App\Models\GameProgress;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
@@ -95,5 +94,67 @@ class SudokuController extends Controller
                 $cellsToRemove--;
             }
         }
+    }
+
+    private function isValidMove($board, $row, $col, $value): bool
+    {
+        if ($value == 0) {
+            return true;
+        }
+
+        if (in_array($value, $board[$row], true)) {
+            return false;
+        }
+
+        for ($i = 0; $i < self::BOARD_SIZE; $i++) {
+            if ($board[$i][$col] == $value) {
+                return false;
+            }
+        }
+
+        $boxRow = floor($row / self::SUB_GRID_SIZE) * self::SUB_GRID_SIZE;
+        $boxCol = floor($col / self::SUB_GRID_SIZE) * self::SUB_GRID_SIZE;
+        
+        for ($i = $boxRow; $i < $boxRow + self::SUB_GRID_SIZE; $i++) {
+            for ($j = $boxCol; $j < $boxCol + self::SUB_GRID_SIZE; $j++) {
+                if ($board[$i][$j] == $value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private function verifySolution($board): bool
+    {
+        for ($i = 0; $i < self::BOARD_SIZE; $i++) {
+            if (!$this->isValidSet(array_slice($board[$i], 0, self::BOARD_SIZE)) || 
+                !$this->isValidSet(array_column($board, $i))) {
+                return false;
+            }
+        }
+
+        for ($boxRow = 0; $boxRow < self::SUB_GRID_SIZE; $boxRow++) {
+            for ($boxCol = 0; $boxCol < self::SUB_GRID_SIZE; $boxCol++) {
+                $box = [];
+                for ($i = 0; $i < self::SUB_GRID_SIZE; $i++) {
+                    for ($j = 0; $j < self::SUB_GRID_SIZE; $j++) {
+                        $box[] = $board[$boxRow * self::SUB_GRID_SIZE + $i][$boxCol * self::SUB_GRID_SIZE + $j];
+                    }
+                }
+                if (!$this->isValidSet($box)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private function isValidSet($set): bool
+    {
+        $uniqueSet = array_unique($set);
+        return count($uniqueSet) == self::BOARD_SIZE && !in_array(0, $uniqueSet, true);
     }
 }
